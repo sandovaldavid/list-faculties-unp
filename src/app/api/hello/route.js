1765be6c1a463a6ade3eb2132c2d43aea0dd68cc
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-09 20:39:38 -0500

    API test initial.

---
+++ src/app/api/hello/route.js
@@ -0,0 +1,8 @@
+import {NextResponse} from "next/server";
+import {pool} from "@/libs/mysql";
+
+export async function GET() {
+  const result = await pool.query("SELECT NOW() as Now");
+  console.log(result[0].Now);
+  return NextResponse.json({message: `Time: ${result[0].Now}`})
+}
