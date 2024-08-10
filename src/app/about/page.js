57f85998591cda919381a0738f93b7b3d3d4f919
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-09 20:39:04 -0500

    About Page #01

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+function AboutPage() {
+  return (
+    <div className="text-center p-10">
+      <h2 className="text-2xl font-bold my-4">About Page</h2>
+      <p className="text-gray-300">
+        lorem3000 is a lorem ipsum generator that generates 3000 characters of lorem ipsum text.
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage;
