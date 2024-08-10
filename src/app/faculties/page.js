4431fbbcb3b1f0a5ef39a23e186314b0f6bb7796
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-10 13:46:31 -0500

    feat(frontend): list faculties ready from facultiesPage

--- src/app/faculties/page.js
+++ src/app/faculties/page.js
@@ -1,6 +1,21 @@
-function FacultiesPage() {
+import axios from 'axios';
+
+async function loadFaculties() {
+  const {data} = await axios.get("http://localhost:3000/api/faculties")
+  return data;
+}
+
+async function FacultiesPage() {
+  const faculties = await loadFaculties();
   return (
-    <p>AllFaculties</p>
+    <div className="grid gap-4 grid-cols-4 ">
+      {faculties.map(faculty => (
+        <div key={faculty.id} className="bg-white rounded-lg border-gray-800 p-4">
+          <h1 className="text-ls font-bold ">{faculty.name}</h1>
+          <p>{faculty.description}</p>
+        </div>
+      ))}
+    </div>
   );
 }
 
