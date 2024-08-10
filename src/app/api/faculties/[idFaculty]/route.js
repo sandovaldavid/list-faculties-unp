5487b771c57a06024fa5f7a1577b8774bb9acfe7
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-10 11:12:19 -0500

    Fix: Method GET #02

--- src/app/api/faculties/[idFaculty]/route.js
+++ src/app/api/faculties/[idFaculty]/route.js
@@ -2,9 +2,15 @@ import {NextResponse} from "next/server";
 import {pool} from "@/libs/mysql.js";
 
 export async function GET(request, {params}) {
-  console.log(params.idFaculty);
-  const result = await pool.query("SELECT * FROM faculties WHERE id = ?", [params.idFaculty]);
-  return NextResponse.json(result[0])
+  try {
+    const result = await pool.query("SELECT * FROM faculties WHERE id = ?", [params.idFaculty]);
+    if (result.length === 0) {
+      return NextResponse.json({message: "Faculty not found"}, {status: 404})
+    }
+    return NextResponse.json(result[0])
+  } catch (e) {
+    return NextResponse.json({error: e.message}, {status: 500})
+  }
 }
 
 export async function DELETE(request, {params}) {
