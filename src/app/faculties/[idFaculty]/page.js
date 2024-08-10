4075e05b8f516adc6478c07ac458b288e35b8ede
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-10 15:17:18 -0500

    feat(frontend): watch faculty card in page

--- src/app/faculties/[idFaculty]/page.js
+++ src/app/faculties/[idFaculty]/page.js
@@ -1,6 +1,26 @@
-function FacultyPage() {
+import axios from "axios";
+import Buttons from "@/components/buttons";
+
+async function loadFacultyId(facultyId) {
+  try {
+    const {data} = await axios.get("http://localhost:3000/api/faculties/" + facultyId)
+    return data;
+  } catch (error) {
+    return error.response.data;
+  }
+}
+
+async function FacultyPage({params}) {
+  const faculty = await loadFacultyId(params.idFaculty);
+  console.log(faculty);
   return (
-    <div>Faculty Page</div>
+    <section className="flex justify-center items-center">
+      <div className="p-6 bg-white  w-1/3 rounded-3xl">
+        <p>Name: {faculty.name}</p>
+        <p>Name: {faculty.description}</p>
+        <Buttons/>
+      </div>
+    </section>
   );
 }
 
