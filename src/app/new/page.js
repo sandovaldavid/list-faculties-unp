5a33272cb704cdca8623cf70b82d5abec072b33f
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-10 13:15:01 -0500

    feat(frontend): add new page for new faculties

---
+++ src/app/new/page.js
@@ -0,0 +1,9 @@
+import FacultyForm from '@/components/FacultyForm';
+
+export default function NewPage() {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <FacultyForm/>
+    </div>
+  );
+}
