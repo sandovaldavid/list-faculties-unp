d3a5db55e5d71116f29a3e7d8f1fa77e637b1f4e
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-10 11:15:23 -0500

    Fix: Method GET #03

--- src/app/api/faculties/route.js
+++ src/app/api/faculties/route.js
@@ -4,6 +4,9 @@ import {pool} from "@/libs/mysql";
 export async function GET() {
   try {
     const results = await pool.query("SELECT * FROM faculties");
+    if (results.length === 0) {
+      return NextResponse.json({message: "No faculties found"}, {status: 404});
+    }
     return NextResponse.json(results);
   } catch (e) {
     return NextResponse.json({error: e.message}, {status: 500});
