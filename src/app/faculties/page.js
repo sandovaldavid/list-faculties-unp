1548e31473fa2c90b98174350703b17195000bc4
Juan David Sandoval Salvador <102970701+sandovaldavid@users.noreply.github.com>
2024-08-13 16:43:31 -0500

    fix(frontend): change API local for connection DB

--- src/app/faculties/page.js
+++ src/app/faculties/page.js
@@ -1,26 +1,30 @@
-"use client"
-import axios from 'axios';
 import FacultyCard from "@/components/facultyCard";
+import { pool } from "@/libs/mysql";
 
 async function loadFaculties() {
   try {
-    const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/faculties`);
-    return data;
+    console.log("Intentando conectar a la base de datos...");
+    const [rows] = await pool.query("SELECT * FROM faculties");
+    console.log("Datos recibidos:", rows);
+    return rows;
   } catch (error) {
     console.error('Error loading faculties:', error);
     return [];  // Retorna un array vacío si hay un error para evitar que la página falle
   }
 }
 
-async function FacultiesPage() {
+export default async function FacultiesPage() {
   const faculties = await loadFaculties();
+  console.log("Facultades cargadas:", faculties);
   return (
     <div className="flex flex-wrap flex-row justify-around mt-8">
-      {faculties.map(faculty => (
-        <FacultyCard faculty={faculty} key={faculty.id}/>
-      ))}
+      {faculties.length > 0 ? (
+        faculties.map(faculty => (
+          <FacultyCard faculty={faculty} key={faculty.id} />
+        ))
+      ) : (
+        <p>No faculties found.</p>
+      )}
     </div>
   );
 }
-
-export default FacultiesPage;
