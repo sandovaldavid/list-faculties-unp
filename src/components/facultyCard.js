8d06fc385e845134d8b1c4270bb51a4c0a594a86
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-13 20:18:36 -0500

    fix(frontend): unused properties

--- src/components/facultyCard.js
+++ src/components/facultyCard.js
@@ -4,12 +4,12 @@ function FacultyCard({faculty}) {
   return (
     <Link className="bg-white rounded-lg border-gray-800 p-4 hover:bg-gray-100 hover:cursor-pointer max-w-60 mb-8"
           href={`/faculties/${faculty.id}`}>
-      {faculty.path_img &&
+      {
+        faculty.path_img &&
         <img className="w-full max-h-40"
              src={faculty.path_img}
-             alt={faculty.name}
-             width={200}
-             height={200}/>}
+             alt={faculty.name}/>
+      }
       <h1 className="text-ls font-bold ">{faculty.name}</h1>
       <p>{faculty.description}</p>
     </Link>
