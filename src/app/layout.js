656ffbfea5964aba09044eb0fc6499ef00ef6319
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-27 02:23:05 -0500

    🐛  fix(layout): adjust layout styles for improved spacing and responsiveness

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,12 +12,12 @@ export const metadata = {
 export default function RootLayout({children}) {
   return (
     <html lang="en">
-    <body className={inter.className}>
-    <Navbar/>
-    <div className="h-[calc(100vh-5rem)] container mx-auto w-full">
-      {children}
-    </div>
-    </body>
+      <body className={inter.className}>
+        <Navbar />
+        <div className="mt-16 min-h-[calc(100vh-4rem)] container mx-auto px-4 py-8">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
