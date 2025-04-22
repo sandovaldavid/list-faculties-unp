eebc1e065c1f24160d31ee114c3c62b9189087a5
sandovaldavid <sandovaldavid2201@gmail.com>
2025-04-22 14:27:19 -0500

    ♻️ refactor University SVG component for consistent attribute casing

--- src/components/icons/University.js
+++ src/components/icons/University.js
@@ -4,16 +4,16 @@ export default function University() {
 			version='1.1'
 			id='UNIVERSITY'
 			xmlns='http://www.w3.org/2000/svg'
-			xmlns:xlink='http://www.w3.org/1999/xlink'
+			xmlnsXlink='http://www.w3.org/1999/xlink'
 			viewBox='0 0 1800 1800'
-			enable-background='new 0 0 1800 1800'
-			xml:space='preserve'
+			enableBackground='new 0 0 1800 1800'
+			xmlSpace='preserve'
 			fill='#000000'>
-			<g id='SVGRepo_bgCarrier' stroke-width='0'></g>
+			<g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
 			<g
 				id='SVGRepo_tracerCarrier'
-				stroke-linecap='round'
-				stroke-linejoin='round'></g>
+				strokeLinecap='round'
+				strokeLinejoin='round'></g>
 			<g id='SVGRepo_iconCarrier'>
 				{' '}
 				<g>
